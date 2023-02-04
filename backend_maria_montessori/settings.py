179487b102d7dd3b96dee4c3dfd0973e3163e3ac
jdevrb <juan.rondon.bonilla@gmail.com>
2023-02-04 18:48:15 -0500

    csrf

--- backend_maria_montessori/settings.py
+++ backend_maria_montessori/settings.py
@@ -172,7 +172,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 CORS_ALLOW_ALL_ORIGINS = True
-#CSRF_TRUSTED_ORIGINS=['https://backendmariamontesori-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://backendmariamontesori-production.up.railway.app']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
